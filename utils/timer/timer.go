40bcdf84c5f18dda7506f02c89d81e010ec4322f
Korbin <86744316@qq.com>
2021-07-19 02:20:30 +0800

    make default format public

--- utils/timer/timer.go
+++ utils/timer/timer.go
@@ -5,8 +5,8 @@ import (
 )
 
 const (
-	defaultFormatLayout       = "2006-01-02 15:04:05"
-	defaultFormat2MilliLayout = "2006-01-02 15:04:05.00"
+	DefaultFormatLayout       = "2006-01-02 15:04:05"
+	DefaultFormat2MilliLayout = "2006-01-02 15:04:05.00"
 )
 
 func Now() string {
@@ -14,19 +14,19 @@ func Now() string {
 }
 
 func Format(t time.Time) string {
-	return t.Format(defaultFormatLayout)
+	return t.Format(DefaultFormatLayout)
 }
 
 func Format2Milli(t time.Time) string {
-	return t.Format(defaultFormat2MilliLayout)
+	return t.Format(DefaultFormat2MilliLayout)
 }
 
 func Parse(s string) (time.Time, error) {
-	return time.ParseInLocation(defaultFormatLayout, s, time.Local)
+	return time.ParseInLocation(DefaultFormatLayout, s, time.Local)
 }
 
 func ParseMilli(s string) (time.Time, error) {
-	return time.ParseInLocation(defaultFormat2MilliLayout, s, time.Local)
+	return time.ParseInLocation(DefaultFormat2MilliLayout, s, time.Local)
 }
 
 func Unix2Time(unix int64) time.Time {
