ff6a023e87293ebd8de84237b36f2402a91dd49f
hezebin <86744316@qq.com>
2022-07-22 11:14:20 +0800

    optimize code struct

--- utils/slicer/delete_test.go
+++ utils/slicer/delete_test.go
@@ -10,15 +10,20 @@ type Stu struct {
 	Age  int    `json:"age" bson:"_age"`
 }
 
-func TestSlice(t *testing.T) {
+func TestDeleteCone(t *testing.T) {
 	s := []int{1, 1, 2, 3, 4, 5, 6, 7, 8, 8, 0}
 	fmt.Println("default s:", s)
-	s = DeleteInt(s, 8)
+	s = Delete(s, 8).([]int)
 	fmt.Println("s delete one 8:", s)
-	s = DeleteInt(s, 1, true)
-	fmt.Println("s delete all 1:", s)
 	stus := []Stu{{"a", 1}, {"b", 2}, {"c", 3}}
 	fmt.Println("struct slicer stus:", stus)
 	stus = Delete(stus, Stu{"a", 1}).([]Stu)
 	fmt.Println("stus delete one Stu{a 1}:", stus)
+}
+
+func TestDeleteAll(t *testing.T) {
+	s := []int{1, 1, 2, 3, 4, 5, 6, 7, 8, 8, 0}
+	fmt.Println("default s:", s)
+	s = DeleteAll(s, 1).([]int)
+	fmt.Println("s delete all 1:", s)
 }
